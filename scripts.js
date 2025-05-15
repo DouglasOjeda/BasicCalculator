bdbf594b25308ffd7bb04588a25d95ef6b310e57
daojeda <daojeda@ncsu.edu>
2025-05-15 16:02:28 -0400

    Add new mod, AC, C, buttons, and event listeners.
    Added new buttons and styled them appropriately.
    I also added event listeners to the buttons.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
+// Better button clicking on mobile
 document.addEventListener('touchstart', () => {}, true);
+
+function printButton(button) {
+    console.log(button.textContent);
+}
+
+const screen = document.getElementById("screen");
+
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => printButton(button));
+})
